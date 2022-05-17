9ce47e98a9a0b605cebac143deee6260d6cf9f39
Benjamin Nordin <godiset@gmail.com>
2022-05-17 12:40:35 +0200

    Details Button Now works

---
+++ WebApplication1/WebApplication1/Controllers/AJAXController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models.People;
+
+namespace WebApplication1.Controllers
+{
+    public class AJAXController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Get(int personid)
+        {
+            PeopleViewModel wm = new PeopleViewModel();
+            Person resultPerson = null;
+            foreach(Person p in wm.getPeople())
+            {
+                if(p.Id == personid)
+                {
+                    resultPerson = p;
+                } else if (p == null)
+                {
+                   resultPerson = wm.getPeople().ElementAt(0);
+                }
+            }
+            return PartialView("/Views/People/_Person.cshtml", resultPerson);
+        }
+
+    }
+}
---
+++ WebApplication1/WebApplication1/wwwroot/media/js/PersonContentManager.js
@@ -0,0 +1,20 @@
+﻿var elementToChange = "#result"
+
+function GetPerson() {
+    let id = document.getElementById("personIDNumber").value;
+    // Send request to backend
+    // Get All Person Objects
+    // Return a partial view for each entry in the element to change
+    let targetElement = document.getElementById(elementToChange);
+    console.log(elementToChange);
+    //let request = $.get("/AJAX/Get?personid=1");
+    $.get("/AJAX/Get?personid=" + id, function (data, status) {
+        $(result).html(data);
+    })
+    console.log("Finished");
+}
+
+function GetAll() {
+    
+
+}
