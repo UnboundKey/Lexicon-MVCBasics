b684bb0bb1c20cd08f6a153545b171e66a3db832
Benjamin Nordin <godiset@gmail.com>
2022-06-28 11:23:43 +0200

    Added convertion object to help with JSON

--- WebApplication1/WebApplication1/Controllers/ReactController.cs
+++ WebApplication1/WebApplication1/Controllers/ReactController.cs
@@ -2,10 +2,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using WebApplication1.Data;
+using WebApplication1.Models.People;
 
 namespace WebApplication1.Controllers
 {
@@ -35,11 +37,15 @@ namespace WebApplication1.Controllers
         public JsonResult GetAll()
         {
             //var dbResult = dbContext.PersonLanguage.Include("Person").Include("Language");
-            var dbResult = dbContext.People.Include("LanguagesLinkObject.Language").Include("PersonCity");
-            var dbresult = dbContext.People.Include(p => p.PersonCity).Include(p=>p.LanguagesLinkObject);
+            var dbResult = dbContext.People.Include(p => p.PersonCity).Include(p=>p.LanguagesLinkObject);
+            
+            //Clean up database data and send it as json
+            List<PersonTO> PeopleCleanedUp = new List<PersonTO>();
+            foreach(Person p in dbResult) {
+                PeopleCleanedUp.Add(new PersonTO(p));
+            }
 
-            var jsoned = Json(dbresult, jsonSerializerOptions);
-
+            var jsoned = Json(PeopleCleanedUp, jsonSerializerOptions);
             return jsoned;
         }
 
@@ -47,7 +53,13 @@ namespace WebApplication1.Controllers
         [Route("api/people/{id}")]
         public JsonResult GetByid(int id) {
             var dbResult = dbContext.People.Include("LanguagesLinkObject.Language").Include("PersonCity").Where(p => p.Id == id);
-            var jsonData = Json(dbResult, jsonSerializerOptions);
+            //Clean up database result and send it as json
+            List<PersonTO> PeopleCleanedUp = new List<PersonTO>();
+            foreach(Person p in dbResult) {
+                PeopleCleanedUp.Add(new PersonTO(p));
+            }
+
+            var jsonData = Json(PeopleCleanedUp, jsonSerializerOptions);
             return jsonData;
         }
 
---
+++ WebApplication1/WebApplication1/Models/People/PersonTO.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using WebApplication1.Models.Languages;
+
+namespace WebApplication1.Models.People
+{
+    public class PersonTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string? Country{ get; set; }
+        public List<Language> Languages;
+
+
+        public PersonTO(Person p) {
+            this.Id = p.Id;
+            this.Name = p.Name;
+            if(p.PersonCity.Country != null)this.Country = p.PersonCity.Country.Name;
+            this.City = p.PersonCity.Name;
+        }
+    }
+}
